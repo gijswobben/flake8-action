b3818b072ed4a9a042684ed3f7c04a8b6ad7b511
Gijs Wobben <gijswobben@gmail.com>
2022-11-28 11:25:06 +0100

    Added grouping of output

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,6 @@ if [ -n "$INPUT_FLAKE8_CONFIG" ]
 then
     echo -e "$INPUT_FLAKE8_CONFIG" > .flake8
 fi
-
 echo "::group::Flake8 config"
 cat .flake8
 echo "::endgroup::"
@@ -13,15 +12,17 @@ echo "::endgroup::"
 # Install additional package (if applicable)
 if [ -n "$ADDITIONAL_PACKAGES" ]
 then
-    echo "Install additional packages"
+    echo "::group::Additional packages"
     pip install -U $ADDITIONAL_PACKAGES
+    echo "::endgroup::"
 fi
 
 # Generate Flake8 output if there isn't any
 if [ -z "$FLAKE8_OUTPUT" ]
 then
-    echo "Running Flake8"
-    flake8 --exit-zero > flake8_results.txt
+    echo "::group::Flake8"
+    flake8 --exit-zero --verbose > flake8_results.txt
+    echo "::endgroup::"
 fi
 
 # Run the command to parse the Flake8 output
