da4ce07b9d53fe6eb61ed98c254734f2222adc32
Gijs Wobben <gijswobben@gmail.com>
2022-11-28 11:05:07 +0100

    Added comments

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,12 +13,14 @@ echo "::endgroup::"
 # Install additional package (if applicable)
 if [ -n "$ADDITIONAL_PACKAGES" ]
 then
+    echo "Install additional packages"
     pip install -U $ADDITIONAL_PACKAGES
 fi
 
 # Generate Flake8 output if there isn't any
 if [ -z "$FLAKE8_OUTPUT" ]
 then
+    echo "Running Flake8"
     flake8 --exit-zero > flake8_results.txt
 fi
 
